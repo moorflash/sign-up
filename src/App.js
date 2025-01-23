2b86becacc0238e10a0320fa8c9e0e35f326d299
moorflash <bmooring222@gmail.com>
2025-01-23 11:57:13 +0000

    inint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
@@ -9,8 +10,34 @@ function App() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
+  function submit(e) {
+    e.preventDefault();
+    const user = {fName, lName, email, password};
+
+    fetch('http://127.0.0.1:8000/user/')
+    .then(() => {
+      console.log(response => response.json())
+    })
+
+    /*fetch('http://127.0.0.1:8000/', {
+      method: 'POST',
+      mode: "no-cors",
+      headers: { Accept: "application/json", "Content-Type": "application/json"},
+      body: JSON.stringify({
+        "first_name": fName,
+        "last_name": lName,
+        "email": email,
+        "password": password
+      })
+
+    }).then(() => {
+      console.log('Successfully added')
+    })
+*/
+  }
+
   return (
-    <form class="sign-up-form">
+    <form class="sign-up-form" onSubmit={(e) => submit(e)}>
       <div class="heading">
           <h1>Sign Up</h1>
         </div>
@@ -19,6 +46,7 @@ function App() {
         type="text"
         required
         placeholder="John"
+        onChange={(e) => setFname(e.target.value)}
         />
       </label><br />
       <label class="lname-field">Last Name<br />
@@ -26,6 +54,7 @@ function App() {
         type="text"
         required
         placeholder="Doe"
+        onChange={(e) => setLname(e.target.value)}
         />
       </label><br />
       <label class="email-field">Email<br />
@@ -33,12 +62,14 @@ function App() {
         type="text"
         required
         placeholder="jdoe@example.com"
+        onChange={(e) => setEmail(e.target.value)}
         />
       </label><br />
       <label class="password-field">Password<br />
         <input 
         type="password"
         required
+        onChange={(e) => setPassword(e.target.value)}
         />
       </label><br />
       <button class="submit-button" type="submit">Sign Up</button>
