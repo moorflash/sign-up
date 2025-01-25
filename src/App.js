9384d95914c4249cc329b07274fa9f396d2209a9
moorflash <bmooring222@gmail.com>
2025-01-25 15:27:25 +0000

    Commit

--- src/App.js
+++ src/App.js
@@ -5,39 +5,40 @@ import axios from 'axios';
 
 function App() {
 
-  const [fName, setFname] = useState();
-  const [lName, setLname] = useState();
+  const [firstName, setFname] = useState();
+  const [lastName, setLname] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
-  function submit(e) {
+   const onSubmit = async (e) => {
     e.preventDefault();
-    const user = {fName, lName, email, password};
 
-    fetch('http://127.0.0.1:8000/user/')
-    .then(() => {
-      console.log(response => response.json())
-    })
+    const data = {
+      firstName,
+      lastName,
+      email,
+      password
+    }
+    
+    const url = "http://127.0.0.1:5000/create_user"
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }
+    const response = await fetch(url, options)
+    if (response.status !== 201 && response.status !== 200) {
+      const data = await response.json()
+      alert(data.message)
+    }
 
-    /*fetch('http://127.0.0.1:8000/', {
-      method: 'POST',
-      mode: "no-cors",
-      headers: { Accept: "application/json", "Content-Type": "application/json"},
-      body: JSON.stringify({
-        "first_name": fName,
-        "last_name": lName,
-        "email": email,
-        "password": password
-      })
-
-    }).then(() => {
-      console.log('Successfully added')
-    })
-*/
+    
   }
 
   return (
-    <form class="sign-up-form" onSubmit={(e) => submit(e)}>
+    <form class="sign-up-form" onSubmit={onSubmit}>
       <div class="heading">
           <h1>Sign Up</h1>
         </div>
@@ -72,7 +73,7 @@ function App() {
         onChange={(e) => setPassword(e.target.value)}
         />
       </label><br />
-      <button class="submit-button" type="submit">Sign Up</button>
+      <button class="submit-button" >Sign Up</button>
     </form>
     
   );
